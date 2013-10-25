a9cd6651d82c7b7f82e6133356bd573d25c0afa2
temkav@yandex.ru <temkav@yandex.ru>
2013-10-25 18:23:58 +0300

    fixing position of elements on dropdown-menu in IE. Required to make some other issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,8 @@ Testtask::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
+  
+  config.assets.paths << "#{Rails.root}/app/assets/images"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
